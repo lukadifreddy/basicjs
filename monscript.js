f31efd2ac8abf51227fa33b4a0f4244fbf685e34
unknown <fredlukadi53@gmail.com>
2024-05-20 12:19:42 +0100

    creer exercice sur l'age dans un tableau

--- monscript.js
+++ monscript.js
@@ -1,12 +1,41 @@
-let age;
-let nom="freddy";
-let etude=false;
+// let age;
+// let nom="freddy";
+// let etude=false;
 const age_valid=18;
-age =prompt("votre age ?");
+// age =prompt("votre age ?");
 
-if(age>=age_valid){
-    alert("Etudiant")
-}
-else{
-    alert("n'es pas etudiant")
+// if(age>=age_valid){
+//     alert("Etudiant")
+// }
+// else{
+//     alert("n'es pas etudiant")
+// };
+
+
+let users=[
+    'freddy',
+    'belamard',
+    'albert',
+    'didier',
+];
+
+let olds=[
+    '20',
+    '32',
+    '15',
+    '18',  
+];
+
+for(let i=0; i<users.length;i++){
+//    olds [i];
+//    alert(users[i]+" age:"+olds[i] );
+      if(olds[i]>=age_valid){
+
+        alert(users[i]+" etudiant "+olds[i]+"ans")
+      }else{
+        alert(users[i]+" n'es pas etudiant "+olds[i]+"ans")
+      }
+        
+
+      
 };
