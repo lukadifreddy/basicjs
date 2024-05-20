bac71a252ca9cf3cee469a472da5b13556793eb6
unknown <fredlukadi53@gmail.com>
2024-05-20 11:22:57 +0100

    first commit

---
+++ monscript.js
@@ -0,0 +1,12 @@
+let age;
+let nom="freddy";
+let etude=false;
+const age_valid=18;
+age =prompt("votre age ?");
+
+if(age>=age_valid){
+    alert("Etudiant")
+}
+else{
+    alert("n'es pas etudiant")
+};
