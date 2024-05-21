0fd5a90a5028ded15d8bb42fea1f90571dab487f
unknown <fredlukadi53@gmail.com>
2024-05-21 14:26:31 +0100

    manipulation de dom

---
+++ domjs.js
@@ -0,0 +1,10 @@
+const header=document.createElement("div");
+header.setAttribute("class","header");
+document.body.appendChild(header);
+const titre=document.createElement("h1");
+titre.textContent="Transfert D'agence";
+header.appendChild(titre);
+const form=document.createElement("form");
+const divparent=document.createElement("div");
+document.body.appendChild(divparent);
+divparent.setAttribute("class","container");
